a18325d2af39070d1820a5454accdeaa33990ec8
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-08-07 19:54:47 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Router } from '@reach/router';
+import Searchbar from './components/Searchbar';
+import Display from './components/Display';
 
 function App() {
+
+  const handleClick = (selected, number) => {
+    fetch(`https://swapi.dev/api/${selected}/${number}`)
+    .then(response => {
+      return response.json();
+    }).then(response => {
+      console.log(response);
+    }).catch(err=>{
+      console.log(err)
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Searchbar handleClick={handleClick}/>
+        <Display/>
     </div>
   );
 }
---
+++ src/components/Searchbar.js
@@ -0,0 +1,37 @@
+import React, {useState } from 'react';
+import { Link } from '@reach/router';
+
+const Searchbar = (props) => {
+
+    const [selected, setSelected] = useState('films');
+    const handleSelect = (event) => {
+        setSelected(event.target.value.toLowerCase())
+    }
+
+    const [number, setNumber] = useState(0);
+    const handleNumber = (e) => {
+        setNumber(e.target.value)
+    }
+
+    return(
+        <div>
+            <label>Search for: </label>
+            <select id="select" name="select" onChange={handleSelect}>
+                <option>Films</option>
+                <option>People</option>
+                <option>Planets</option>
+                <option>Species</option>
+                <option>Starships</option>
+                <option>Vehicles</option>
+            </select>
+
+            <label>ID: </label>
+            <input type="number" value={number} onChange={handleNumber}></input>
+
+            <button onClick={() => props.handleClick(selected, number)}>Search</button>
+
+        </div>
+    );
+};
+
+export default Searchbar;
